6610693e93fee35f33056c6b6690e8001540668e
Martynas Jakutis <jakumart@gmail.com>
2024-02-26 19:35:54 +0200

    js

---
+++ javascript/1 pamoka/js/script.js
@@ -0,0 +1,36 @@
+let d = new Date();
+document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+
+let message = 50;
+let name = "Martynas";
+
+
+// Numbers 
+const birthYear = 1996;
+const number1 = -55;
+const nuber2 = 2.354543;
+
+// Strings 
+const string1 = "Hello";
+const string2 = "kazkas";
+const string3 = "121212";
+const string4 = `hello`;
+// kaip string kabutes galima naudoti ir `` (backtick)
+
+// Boolean 
+const bool1 = true;
+const bool2 = false;
+
+// null 
+const null1 = null;
+
+// undefined
+const undefined1 = undefined;
+
+message = name
+
+// alert(message)
+
+// message = 100;
+
+// alert(message)
