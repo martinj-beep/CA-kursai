c0e098d732d23339327b5e94ad9a4fb8c0d96327
Martynas Jakutis <jakumart@gmail.com>
2024-02-28 19:58:00 +0200

    troeiaj

---
+++ javascript/3 pamoka/teorine dalis/script.js
@@ -0,0 +1,153 @@
+// const userInput = prompt("iveskite skaiciu");
+
+// if (userInput > 10) { // kadangi true, kodas bus vykdomas
+//     console.log("ivestas skaicius didesnis negu 10");
+// } else { //veiks, tik kuomet if salyga nebus patenkinta
+//     console.log("ivestas skaicius mazesnis nei 10");
+// }
+
+// const isRaining = true;
+
+// if (isRaining) {
+//     console.log("pasiimkite sketi");
+// }
+
+// const temperature = +prompt("iveskite temperatura");
+
+// if (temperature >= 30) {
+//     console.log("lauke itin karsta");
+// } else if (temperature >= 20) {
+//     console.log("lauke silta");
+// } else if (temperature >= 10) {
+//     console.log("lauke vesu");
+// } else {
+//     console.log("trauk ziemine striuke");
+// }
+
+// if (temperature > 20) {
+//     console.log("galima gyventi");
+// } else {
+//     console.log("nepatartina gyvent");
+// }
+
+// temperature > 20 ? console.log("galima gyvernti") : console.log("nepatartina gyventi");
+
+// const isLoggedIn = true;
+// const output = isLoggedIn == true ? console.log("sveiki") : console.log("nesveiki");
+
+
+
+
+
+// switch (temperature) {
+//     case 20:
+//         console.log("lauke silta");
+//         break;
+//     case 30:
+//         console.log("lauke karsta");
+//         break;
+//     default:
+//     console.log("neturiu nuomones");
+// }
+
+
+
+
+
+// const isNotRaining = true;
+
+// if (temperature > 20 && isNotRaining) {
+//     console.log("siandien grazi diena, nes nelija ir silta");
+// } else {
+//     console.log("ne itin grazi diena");
+// }
+
+// if (temperature > 20 || isNotRaining) {
+//     console.log("gera dienuke, nes nelija arba yra silta");
+// }
+
+// const isRaining = true;
+
+// if (temperature > 20 && !isRaining) {
+//     console.log("siandien grazi diena, nes nelija ir silta");
+// }
+
+
+
+
+
+// Username and Password Checker:Implement a basic username and password checker. Ask the user for their username and password. If the username is "admin" and the password is "password123", display a message saying "Login successful". Otherwise, display a message saying "Incorrect username or password".
+
+// const username = "admin";
+// const password = "password123"; 
+
+// const enteredUsername = prompt("iveskite savo vartotojo varda");
+// const enteredPassword = prompt("iveskite savo slaptazodi");
+
+// if (enteredUsername === username && enteredPassword === password) {
+//     console.log("login succesfull");
+// } else {
+//     console.log("incorrect username or password");
+// }
+
+
+
+
+// Even or Odd Number Checker:Write a program that asks the user for a number and determines whether it's even or odd. Display a message indicating whether the number is even or odd.
+
+// const userInput = +prompt("iveskite skaiciu");
+
+// if (userInput % 2 === 0) { // palygins, ar dalinasi be liekanos is dvieju - jeigu reazultatas 0 (dalinasi is 2 be liekanos), tuomet true
+//     console.log("ivestas skaicius yra lyginis");
+// } else {
+//     console.log("ivestas skaicius yra nelyginis");
+// }
+
+
+
+
+// Divisibility Checker:Write a program that asks the user for a number and checks if it's divisible by both 3 and 5. Display a message indicating whether the number is divisible by both 3 and 5.
+
+// const userInput = +prompt("iveskite skaiciu");
+
+// if (userInput % 3 === 0 && userInput % 5 ===0) {
+//     console.log("skaicius dalinasi ir is 3 ir is 5");
+// } else {
+//     console.log("skaicius nesidalina is abieju skaiciu");
+// }
+
+
+// FIZZ BUZZ
+// /3 - FIZZ; /5 - BUZZ; /3 IR /5 FIZZBUZZ
+
+// const userInput = +prompt("iveskite skaiciu");
+
+// if (userInput % 3 === 0 && userInput % 5 === 0) {
+//     console.log("fizzbuzz");
+// } else if (userInput % 3 === 0) {
+//     console.log("fizz");
+// } else if (userInput % 5 === 0) {
+//     console.log("buzz");
+// } 
+
+
+
+
+// Grading System:Create a program that asks the user for their test score. Based on the score, display a letter grade according to the following criteria:
+// 90 or above: A
+// 80-89: B
+// 70-79: C
+// 60-69: D
+// Below 60: F
+
+// const usersGrade = +prompt("Iveskite pazymi");
+
+// let grade;
+
+// if (usersGrade >= 90) {
+//     grade = "A"
+// } else if (usersGrade >= 80 && usersGrade <= 89) {
+//     grade = "B"
+// }
+
+// console.log(grade);
