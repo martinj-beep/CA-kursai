fbdf2e39b37dc345e9da7409c6d401e6e6626c19
Martynas Jakutis <jakumart@gmail.com>
2024-03-03 17:48:52 +0200

    praktika

--- javascript/5 pamoka/praktine dalis/script.js
+++ javascript/5 pamoka/praktine dalis/script.js
@@ -1,2 +1,34 @@
-let age = 25;
-console.log(2023 - age);
+// let age = 25;
+// console.log(2023 - age);
+
+// const name = "martynas";
+// const count = 10;
+
+// for (let i = 0; i < count; i++) {
+//     console.log(`${i}. ${name}`);
+// }
+
+// for (let i = 10; i >= 1; i--) {
+//     console.log(i);
+// }
+// let i = 0;
+// const name = "martynas"
+
+// while (i < 3) {
+//     i++;
+//     console.log(name);
+// }
+
+
+// Sukurkite kintamąjį let combo = ''. Kiekvieną kartą paleidus ciklą į combo prisirašo jūsų vardas (t.y. while ciklo viduje darytų combo += "Petras" ). Sukurkite programą, kur bus kintamasis let times = 5, kuris nurodys kiek kartų į combo kintamąjį turėtų būti pridėtas jūsų vardas. Jei times skaičius 0 arba mažiau – vis tiek bent vieną kartą įvyks ciklas ir jūsų vardą įrašys į combo.
+
+let combo = "";
+let times = 4;
+let name = "Martynas";
+
+do {
+    combo += name;
+    times--;
+} while (times > 0);
+
+console.log(combo);
