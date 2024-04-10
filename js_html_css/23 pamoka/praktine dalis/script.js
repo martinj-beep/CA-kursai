2c379a7a44afe2fba171d84ff3e87cfb0602e3ea
Martynas Jakutis <jakumart@gmail.com>
2024-04-10 22:54:15 +0300

    rgdrg

---
+++ js_html_css/23 pamoka/praktine dalis/script.js
@@ -0,0 +1,7 @@
+const server = "https://polar-chartreuse-silverfish.glitch.me";
+
+//     Sukurkite lentelę naudojant JavaScript.
+
+// Į lentelę įrašykite duomenis: id, name, city, fav_color.
+
+const table = document.createElement("table")
